eae6187b5b57b43d64fb828ec924014cefa2c975
Jefferson Duarte <jeffersonsduarte@live.com>
2022-07-04 12:09:43 +0100

    Adicionado validação do password

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+import re
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .utils import password_is_valid
+
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
-
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    elif request.method == 'POST':
+        usuario = request.POST.get('usuario')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        
+        if not password_is_valid(request, senha, confirmar_senha):
+            return redirect('/auth/cadastro/')
+        
+        return HttpResponse(f'Usuraio: {usuario} - Email: {email} - Senha: {senha} - Confirmar Senha: {confirmar_senha}')
+    
 def logar(request):
     return HttpResponse('Logar')
