064f16834ca8efc72857aa38ac3324c12a7644b1
Jefferson Duarte <jeffersonsduarte@live.com>
2022-07-04 11:22:19 +0100

    Adicionado tela de cadastro

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-    return HttpResponse('Cadastro')
+    return render(request, 'cadastro.html')
+
+def logar(request):
+    return HttpResponse('Logar')
